95efd7c6a909f75e9090f5fad6b3a343d2f697a0
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-09 12:39:52 +0530

    Added : Algorithm Practice Programs

---
+++ src/PrimeNumber.java
@@ -0,0 +1,12 @@
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println();
+
+        String primeNumbers = "";
+        System.out.println("Printing prime numbers between 0-1000 ");
+        for (int i = 1; i <= 1000;i++) {
+            int count = 0;
+
+    }
+}}
